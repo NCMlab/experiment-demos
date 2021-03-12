83800ae3747a35c89823f0546ccb7a8419834c88
Jason Steffener - laptop <steffener@gmail.com>
2021-03-11 21:53:11 -0500

    Create BGR_Setup_EN.js

---
+++ BGR/BGR_Setup_EN.js
@@ -0,0 +1,260 @@
+
+var BaddGramReasonList = [
+[
+  {
+    "Sentence": "A is preceded by B",
+    "Response": "BA"
+  },
+  {
+    "Sentence": "B does not precede A",
+    "Response": "AB"
+  },
+  {
+    "Sentence": "A is not followed by B",
+    "Response": "BA"
+  },
+  {
+    "Sentence": "B is preceded by A",
+    "Response": "BA"
+  },
+  {
+    "Sentence": "A is followed by B",
+    "Response": "AB"
+  },
+  {
+    "Sentence": "A does not follow B",
+    "Response": "AB"
+  },
+  {
+    "Sentence": "B is not preceded by A",
+    "Response": "AB"
+  },
+  {
+    "Sentence": "B follows A",
+    "Response": "AB"
+  },
+  {
+    "Sentence": "A precedes B",
+    "Response": "BA"
+  },
+  {
+    "Sentence": "B does not follow A",
+    "Response": "BA"
+  },
+  {
+    "Sentence": "B precedes A",
+    "Response": "AB"
+  },
+  {
+    "Sentence": "B is followed by A",
+    "Response": "AB"
+  },
+  {
+    "Sentence": "B is not followed by A",
+    "Response": "BA"
+  },
+  {
+    "Sentence": "B is preceded by A",
+    "Response": "AB"
+  },
+  {
+    "Sentence": "B is followed by A",
+    "Response": "BA"
+  },
+  {
+    "Sentence": "B precedes A",
+    "Response": "BA"
+  },
+  {
+    "Sentence": "A is not followed by B",
+    "Response": "AB"
+  },
+  {
+    "Sentence": "A is followed by B",
+    "Response": "BA"
+  },
+  {
+    "Sentence": "B is not preceded by A",
+    "Response": "BA"
+  },
+  {
+    "Sentence": "B is followed by A",
+    "Response": "AB"
+  },
+  {
+    "Sentence": "A does not follow B",
+    "Response": "BA"
+  },
+  {
+    "Sentence": "A is preceded by B",
+    "Response": "AB"
+  },
+  {
+    "Sentence": "B does not follow A",
+    "Response": "AB"
+  },
+  {
+    "Sentence": "A is not preceded by B",
+    "Response": "BA"
+  },
+  {
+    "Sentence": "A follows B",
+    "Response": "BA"
+  },
+  {
+    "Sentence": "A is not preceded by B",
+    "Response": "AB"
+  },
+  {
+    "Sentence": "A follows B",
+    "Response": "AB"
+  },
+  {
+    "Sentence": "A does not precede B",
+    "Response": "AB"
+  },
+  {
+    "Sentence": "A precedes B",
+    "Response": "AB"
+  },
+  {
+    "Sentence": "B follows A",
+    "Response": "BA"
+  },
+  {
+    "Sentence": "B does not precede A",
+    "Response": "BA"
+  },
+  {
+    "Sentence": "A does not precede B",
+    "Response": "BA"
+  },
+  {
+    "Sentence": "A does not follow B",
+    "Response": "AB"
+  },
+  {
+    "Sentence": "A is not followed by B",
+    "Response": "BA"
+  },
+  {
+    "Sentence": "B is not preceded by A",
+    "Response": "BA"
+  },
+  {
+    "Sentence": "B is preceded by A",
+    "Response": "AB"
+  },
+  {
+    "Sentence": "A follows B",
+    "Response": "BA"
+  },
+  {
+    "Sentence": "B precedes A",
+    "Response": "BA"
+  },
+  {
+    "Sentence": "B is followed by A",
+    "Response": "BA"
+  },
+  {
+    "Sentence": "A precedes B",
+    "Response": "AB"
+  },
+  {
+    "Sentence": "A follows B",
+    "Response": "AB"
+  },
+  {
+    "Sentence": "B does not precede A",
+    "Response": "BA"
+  },
+  {
+    "Sentence": "A does not precede B",
+    "Response": "BA"
+  },
+  {
+    "Sentence": "A is preceded by B",
+    "Response": "BA"
+  },
+  {
+    "Sentence": "B is not followed by A",
+    "Response": "AB"
+  },
+  {
+    "Sentence": "B does not follow A",
+    "Response": "BA"
+  },
+  {
+    "Sentence": "B does not precede A",
+    "Response": "AB"
+  },
+  {
+    "Sentence": "A is followed by B",
+    "Response": "AB"
+  },
+  {
+    "Sentence": "B is not preceded by A",
+    "Response": "AB"
+  },
+  {
+    "Sentence": "B follows A",
+    "Response": "BA"
+  },
+  {
+    "Sentence": "A does not precede B",
+    "Response": "AB"
+  },
+  {
+    "Sentence": "A is not followed by B",
+    "Response": "AB"
+  },
+  {
+    "Sentence": "B is preceded by A",
+    "Response": "BA"
+  },
+  {
+    "Sentence": "A is not preceded by B",
+    "Response": "AB"
+  },
+  {
+    "Sentence": "A does not follow B",
+    "Response": "BA"
+  },
+  {
+    "Sentence": "A is followed by B",
+    "Response": "BA"
+  },
+  {
+    "Sentence": "A is not preceded by B",
+    "Response": "BA"
+  },
+  {
+    "Sentence": "B is followed by A",
+    "Response": "AB"
+  },
+  {
+    "Sentence": "B is not followed by A",
+    "Response": "BA"
+  },
+  {
+    "Sentence": "A precedes B",
+    "Response": "BA"
+  },
+  {
+    "Sentence": "B does not follow A",
+    "Response": "AB"
+  },
+  {
+    "Sentence": "A is preceded by B",
+    "Response": "AB"
+  },
+  {
+    "Sentence": "B precedes A",
+    "Response": "AB"
+  },
+  {
+    "Sentence": "B follows A",
+    "Response": "AB"
+  }
+]
