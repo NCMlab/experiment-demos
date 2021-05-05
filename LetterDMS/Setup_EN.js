e4efc182b5c61151c53ba728d08fe57c70eacc2a
Jason Steffener - laptop <steffener@gmail.com>
2021-05-04 22:06:38 -0400

    making a staircase function

--- LetterDMS/Setup_EN.js
+++ LetterDMS/Setup_EN.js
@@ -1,1 +1,14 @@
 function PutLetersInGrid(LetterList)
+	{}
+
+function Staircase(ResponseCorrect=1, Current=1, MinValue=1, MaxValue=10, MaxReversals=5, MaxTrials=40, StepSize=1){
+	var feedback = ""
+	if ResponseCorrect == 1 {
+		feedback = "CORRECT"
+	} else {
+		feedback =  "Incorrect"
+	}
+	return feedback
+}
+
+
