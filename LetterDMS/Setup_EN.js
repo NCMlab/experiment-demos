309094b51dad3ec57c89703d5d475b1bc86c394f
Jason Steffener - laptop <steffener@gmail.com>
2021-05-24 12:47:11 -0400

    adding comments

--- LetterDMS/Setup_EN.js
+++ LetterDMS/Setup_EN.js
@@ -1,242 +1,51 @@
 // General parameters used by all versions of the DFMS task
+// The following letter list does not include vowels nor w. W is removed because it is the only two syllable 
+// letter in the English language. During subvocal rehearsal W therefore takes two syllables to rehearse.
+// The letter ELL is in included as a stimulus letter but NOT a probe letter. This is done so that proactive
+// interference can be minimized between consecutive trials with a load of 9 letters. Without ELL as a stimulus
+// there are simply not enough available letters. ELL is not allowed to be a probe leter because it is difficult 
+// to differentiate it from ONE and EYE.
+
 var AllowableLetters = "BCDFGHJKLMNPQRSTVXYZ";
 
 var FullScreenMode = false;
 
-
-var KeyboardChoices = ['arrowleft', 'arrowright'];
-
 var FontSize = 72;
 
+var KeyboardChoices = ['arrowleft', 'arrowright'];
 // the following is used for scoring and allows the keyboard choices to be whatever you would like
+// This response mapping correspopnds to the order of the above KeyboardChoices
 var ResponseMapping = [true, false];
 
+// Timings of the different trial phases in MILLISECONDS
 var StimOnTime = 2500
 var RetOnTime = 3500
 var ProbeOnTime = 5000
-// This is the intertrial interval. This experimental component is part of the trial.
+// This is the intertrial interval. 
 var ITITime = 1000
+
 // This is the time between blocks. Note that between each block of trials there
 // is also the 3-2-1 countdown. Therefore, the full interblock interval is this value PLUS 
 // the countdown time, which is 3 seconds.
 var InterBlockTime = 3000
+// TO DO ++++++
 // This is a delay component for use after instructions and before the first Block and at the
 // the end before the thank you screen
-ShortDelayTime = 30
-NumberOfBlocks = 5
-TrialsPerBlock = 6
+//ShortDelayTime = 30
 
 
-// Block based parameters
+
+// ===================================================
+// BLOCK BASED PARAMETERS
+// How many trials per block
 var NTrialsPerBlock = 6;
-
-
-// Adaptive difficulty parameters
+// The number of blocks to run
+NumberOfBlocks = 5
+// ===================================================
+// ADAPTIVE VERSION PARAMETERS
 var MaxTrials = 50
 var MaxReversals = 10
-
-var CurrentLetterList = '';
-
-function PutLettersInGrid(LetterList,NRows,NCols, width=600, height=300, FontSize=40)
-	{
-		var count = 0;
-		var Table = '';
-		// create the html table and assign the class which is defined in the HTML file
-		Table += '<table class="a">'
-		for (var i=0; i<NRows; i++) { // Cycle over rows
-			Table += '<tr height="'+height+'">'
-			for (var j=0; j<NCols; j++) { // cycle over columns
-				Table += '<td style="font-size:'+FontSize+'px; line-width:100px">'+LetterList[count]+'</td>';
-				count += 1;
-			}
-		}
-		return Table
-	}
-
-function CountdownTimer(MillisecondsPerNumber = 1000)
-	{
-    var Three = {
-      type: 'html-keyboard-response',
-      stimulus: '<p>3</p>',
-      choices: jsPsych.NO_KEYS,
-      trial_duration: MillisecondsPerNumber,
-      }
-    var Two = {
-      type: 'html-keyboard-response',
-      stimulus: '<p>2</p>',
-      choices: jsPsych.NO_KEYS,
-      trial_duration: MillisecondsPerNumber,
-      }
-    var One = {
-      type: 'html-keyboard-response',
-      stimulus: '<p>1</p>',
-      choices: jsPsych.NO_KEYS,
-      trial_duration: MillisecondsPerNumber,
-      }
-
-    var Countdown_procedure = {
-      // Make sure this order is correct: fixation cue and then the stimulus
-      // Otherwise the scoring will not make any sense
-      timeline: [Three, Two, One],
-      randomize_order: false  
-    }
-    return Countdown_procedure
-    } 
-	
-
-
-function RemoveOldLetters(AllowableLetters, LastTrialStimulus, LastTrialProbe)
-	{	// remove the letters from the last trial from teh list of allowable letters
-		// For the first trial there are no previous stimuli, so check for that
-		if (typeof LastTrialStimulus !== 'undefined')
-		{
-			for (var i=0; i < LastTrialStimulus.length; i++) {
-				AllowableLetters = AllowableLetters.replace(LastTrialStimulus[i],'');
-			}
-			// remove the probe letter also, after converting it to uppercase
-			AllowableLetters = AllowableLetters.replace(LastTrialProbe.toUpperCase(),'')
-		}
-		else {
-			AllowabelLetters = AllowableLetters
-		}
-		return AllowableLetters
-	}
-
-function MakeStimulus(LettersToUse, Load)
-	{
-		// Make a letter list for use as stimuli
-		// Shuffle the letters
-		// Make sure a load 1 letter is not L
-		var IsEll = true
-		while (IsEll) {
-			var ShuffledLetters = shuffle(LettersToUse)
-			//console.log(LettersToUse)
-			// Onky take the required number of letters based on the load
-			var LetterString = ShuffledLetters.substring(0,Load)
-			console.log(LetterString)
-			if (Load == 1 & LetterString == "L") {
-				IsEll = true
-			}
-			else {IsEll = false}
-		}
-		return LetterString
-	}	
-
-function CreateProbeLetter(CurrentStim, AllowableLetters)
-	{
-		ProbeType = MakeProbeType()
-
-		if (ProbeType == 1)
-		{
-			//LookingForProbe = true
-			//while (LookingForProbe)
-			//{
-				// select a random letter from the current stim
-				// ADD CHCK TO MAKE SURE ELL IS NOT THE PROBE
-				var IsEll = true
-				while (IsEll) {
-					ShuffledStim = shuffle(CurrentStim)
-					CurrentProbe = ShuffledStim[0].toLowerCase()
-					console.log('Trying: '+CurrentProbe)
-					if (CurrentProbe != 'l') {
-						IsEll = false
-					}
-				}
-				var correct = true
-			//	if (CurrentProbe != "L")
-			//	{LookingForProbe = false}
-			//}
-		}
-		else 
-		{ // Remove the current stim letters from the available letter set
-        	CurrentAllowableList = RemoveOldLetters(AllowableLetters, CurrentStim, '')
-			var IsEll = true
-			while (IsEll) {
-				ShuffledStim = shuffle(CurrentAllowableList)
-				CurrentProbe = ShuffledStim[0].toLowerCase()
-				// Check to make sure the probe letter is NOT ell
-				if (CurrentProbe != 'l') {
-					IsEll = false
-				}			
-			}
-			var correct = false
-		}
-		return [CurrentProbe, correct]
-	}
-function MakeProbeType()
-	{
-		// Decide if this is a posiitve (1) or negative (0) trial
-		return Math.round(Math.random())
-	}
-function getRandomInt(n) 
-	{ //https://www.codespeedy.com/shuffle-characters-of-a-string-in-javascript/
-  		return Math.floor(Math.random() * n);
-	}
-
-function shuffle(s) {
-	  var arr = s.split('');           // Convert String to array
-	  var n = arr.length;              // Length of the array
-	  
-	  for(var i=0 ; i<n-1 ; ++i) {
-	    var j = getRandomInt(n);       // Get random of [0, n-1]
-	   
-	    var temp = arr[i];             // Swap arr[i] and arr[j]
-	    arr[i] = arr[j];
-	    arr[j] = temp;
-	  }
-	  s = arr.join('');                // Convert Array to string
-	  return s;                        // Return shuffled string
-	}
-
-function MakeAdaptiveStimulus(Load, LastTrialStimulus, LastTrialProbe)
-	// Make stimuli on-the-fly and make sure that no current letters were included in the previous trial
-	{
-		// Remove letters from the last trial
-		//console.log(AllowableLetters)
-		LettersToUse = RemoveOldLetters(AllowableLetters, LastTrialStimulus, LastTrialProbe)
-		// Shuffle the remaining letters
-		// Select an appropriate length of letters according to the load
-		LetterString = MakeStimulus(LettersToUse, Load)
-		// Decide if the probe is positive or negative
-		CurrentProbe  = CreateProbeLetter(LetterString, LettersToUse)
-		// Pad Letter String
-		Stimulus = PadLetters(LetterString)
-		return [Stimulus, CurrentProbe, LetterString]
-	}
-
-function PadLetters(Letters)
-	{
-		switch (Letters.length) {
-			case 1:
-				Stimulus = '****'+Letters+'****';
-				break;
-			case 2:
-				Stimulus = '***'+Letters[0]+'*'+Letters[1]+'***';
-				break;
-			case 3:
-				Stimulus = '***'+Letters.slice(0,3)+'***';
-				break;
-			case 4:
-				Stimulus = Letters[0]+'*'+Letters[1]+'***'+Letters[2]+'*'+Letters[3];
-				break;
-			case 5:
-				Stimulus = Letters[0]+'*'+Letters[1]+'*'+Letters[2]+'*'+Letters[3]+'*'+Letters[4];
-				break;
-			case 6:
-				Stimulus = Letters.slice(0,3)+'***'+Letters.slice(3,6)
-				break;
-			case 7:
-				Stimulus = Letters.slice(0,3)+'*'+Letters[3]+'*'+Letters.slice(4,7);
-				break;
-			case 8:
-				Stimulus = Letters.slice(0,4)+'*'+Letters.slice(4,8)
-				break;
-			case 9:
-				Stimulus = Letters.slice(0,9)
-		}
-		return Stimulus
-	}
-
+// ===================================================
 var instructions = ['<p>Press [LEFT] if the letter WAS in the set.<br>Press [RIGHT] if the letter WAS NOT in the set.<br>',
 	'<p>You will NOT receive feedback after each trial.</p>',
 	'Remember that the letters to study will be in white and CAPITALIZED.',
@@ -244,7 +53,13 @@ var instructions = ['<p>Press [LEFT] if the letter WAS in the set.<br>Press [RIG
 	'Try to respond as quickly and as accurately as possible.',
 	'Press the any key to begin.']
 
-//TL,TM,TR,CL,CM,CR,BL,BM,BR,probe,corr,Load
+
+
+
+// The follopwing are stimulus lists that are carefully selected to be counterbalanced for 
+// probe type and to have six trials per block and minimal proactive interference from 
+// previous trials
+
 var DMSLetterBehaviorList001 = [
 	{"StimulusLetters": "****G****", "ProbeLetter": "g", "Correct": true, "Load": 1},
 	{"StimulusLetters": "****H****", "ProbeLetter": "h", "Correct": true, "Load": 1},
