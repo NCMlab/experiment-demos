8e7f410e97e89a824419ed9c411e06c8ed47d33a
Jason Steffener NCMLab2 <steffener@gmail.com>
2021-05-24 21:57:17 -0400

    Finished BDI and added CUDIT-R

--- LikertQuestionnaires/BeckDepIndex.js
+++ LikertQuestionnaires/BeckDepIndex.js
@@ -1,11 +1,19 @@
+var title = "Beck Depression Index"
+
+var instructions = "This questionnaire consists of 21 groups of statements. Please read each group of statements carefully. And then pick out the one statement in each group that best describes the way you have been feeling during the past two weeks, including today. Circle the number beside the statement you have picked. If several statements in the group seem to apply equally well, circle the highest number for that group."
+
+var reference = ""
 
 
 var items = [
   {
     prompt: "Sadness", 
     name: "Sadness", 
-    options: ["I do not feel sad.", "I feel sad.", "I am sad all the time and I can't snap out of it.", "I am so sad and unhappy that I can't stand it."], 
-    required:false
+    options: ["I do not feel sad.", 
+          "I feel sad.", 
+          "I am sad all the time and I can't snap out of it.", 
+          "I am so sad and unhappy that I can't stand it."], 
+    required:true
   },
   {
     prompt: "Pessimism",
@@ -14,7 +22,7 @@ var items = [
           "I feel discouraged about the future.",
           "I feel I have nothing to look forward to.",
           "I feel the future is hopeless and that things cannot improve."],
-    required: false
+    required: true
   },
   {
   	prompt: "Past Failure",
@@ -24,7 +32,7 @@ var items = [
 			"As I look back on my life, all I can see is a lot of failures.",
 			"I feel I am a complete failure as a person."
 			],
-  	required: false
+  	required: true
   },
   {
   	prompt: "Loss of Pleasure",
@@ -33,7 +41,7 @@ var items = [
  			"I don't enjoy things the way I used to.",
  			"I don't get real satisfaction out of anything anymore.",
  			"I am dissatisfied or bored with everything."],
- 	required: false
+ 	required: true
   },
   {
   	prompt: "Guilty Feelings",
@@ -42,7 +50,7 @@ var items = [
  			"I feel guilty a good part of the time.",
  			"I feel quite guilty most of the time.",
  			"I feel guilty all of the time."],
-  	required: false
+  	required: true
   },
   {
   	prompt: "Punishment Feelings",
@@ -51,7 +59,7 @@ var items = [
  			"I feel I may be punished.",
  			"I expect to be punished.",
  			"I feel I am being punished."],
-  	required: false
+  	required: true
   },
   {
   	prompt: "Self-Dislike",
@@ -60,7 +68,7 @@ var items = [
  			"I am disappointed in myself.",
  			"I am disgusted with myself.",
  			"I hate myself."],
-  	required: false
+  	required: true
   },
   {
   	prompt: "Self-Criticalness",
@@ -69,7 +77,7 @@ var items = [
  			"I am critical of myself for my weaknesses or mistakes.",
  			"I blame myself all the time for my faults.",
  			"I blame myself for everything bad that happens."],
-  	required: false
+  	required: true
   },
     {
   	prompt: "Suicidal Thoughts or Wishes",
@@ -78,7 +86,7 @@ var items = [
  			"I have thoughts of killing myself, but I would not carry them out.",
  			"I would like to kill myself.",
  			"I would kill myself if I had the chance."],
-  	required: false
+  	required: true
   },
   {
   	prompt: "Crying",
@@ -87,7 +95,7 @@ var items = [
  			"I cry more now than I used to.",
  			"I cry all the time now.",
  			"I used to be able to cry, but now I can't cry even though I want to."],
-  	required: false
+  	required: true
   },
   {
   	prompt: "Agitation",
@@ -96,7 +104,7 @@ var items = [
  			"I am slightly more irritated now than usual.",
  			"I am quite annoyed or irritated a good deal of the time.",
  			"I feel irritated all the time."],
-  	required: false
+  	required: true
   },
   {
   	prompt: "Loss of Interest",
@@ -105,7 +113,7 @@ var items = [
  			"I am less interested in other people than I used to be.",
  			"I have lost most of my interest in other people.",
  			"I have lost all of my interest in other people."],
-  	required: false
+  	required: true
   },
   {
   	prompt: "Indecisiveness",
@@ -114,7 +122,7 @@ var items = [
  			"I put off making decisions more than I used to.",
  			"I have greater difficulty in making decisions more than I used to.",
  			"I can't make decisions at all anymore."],
-  	required: false
+  	required: true
   },
   {
   	prompt: "Worthlessness",
@@ -123,7 +131,7 @@ var items = [
  			"I am worried that I am looking old or unattractive.",
  			"I feel there are permanent changes in my appearance that make me look unattractive.",
  			"I believe that I look ugly."],
-  	required: false
+  	required: true
   },
   {
   	prompt: "Loss of Energy",
@@ -132,7 +140,7 @@ var items = [
  			"It takes an extra effort to get started at doing something.",
  			"I have to push myself very hard to do anything.",
  			"I can't do any work at all."],
-  	required: false
+  	required: true
   },
   {
   	prompt: "Changes in Sleeping Pattern",
@@ -141,7 +149,7 @@ var items = [
  			"I don't sleep as well as I used to.",
  			"I wake up 1-2 hours earlier than usual and find it hard to get back to sleep.",
  			"I wake up several hours earlier than I used to and cannot get back to sleep."],
-  	required: false
+  	required: true
   },
   {
   	prompt: "Tiredness or Fatigue",
@@ -150,7 +158,7 @@ var items = [
  			"I get tired more easily than I used to.",
  			"I get tired from doing almost anything.",
  			"I am too tired to do anything."],
-  	required: false
+  	required: true
   },
   {
   	prompt: "Changes in Appetite",
@@ -159,7 +167,7 @@ var items = [
  			"My appetite is not as good as it used to be.",
  			"My appetite is much worse now.",
  			"I have no appetite at all anymore."],
-  	required: false
+  	required: true
   },
   {
   	prompt: "Weight Loss",
@@ -168,7 +176,7 @@ var items = [
  			"I have lost more than five pounds.",
  			"I have lost more than ten pounds.",
  			"I have lost more than fifteen pounds."],
-  	required: false
+  	required: true
   },
   {
   	prompt: "Personal Health",
@@ -177,7 +185,7 @@ var items = [
  			"I am worried about physical problems like aches, pains, upset stomach, or constipation.",
  			"I am very worried about physical problems and it's hard to think of much else.",
  			"I am so worried about my physical problems that I cannot think of anything else."],
-  	required: false
+  	required: true
   },
   {
   	prompt: "Loss of Interest in Sex",
@@ -186,7 +194,7 @@ var items = [
  			"I am less interested in sex than I used to be.",
  			"I have almost no interest in sex.",
  			"I have lost interest in sex completely."],
-  	required: false
+  	required: true
   }
   ]
 
